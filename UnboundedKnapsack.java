502e088391fbfd3f58bcd0f0b115664079c175b2
mskarthi125 <51087854+mskarthi125@users.noreply.github.com>
2019-07-08 12:04:49 +1000

    UnboundedKnapsack Solution

--- UnboundedKnapsack.java
+++ UnboundedKnapsack.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 //Given an array of integers and a target sum, determine the sum nearest to but not exceeding the target that can be created. 
 //To create the sum, use any element of your array zero or more times.
-public class Test2Programming {
+public class UnboundedKnapsack {
 
 	private static final Scanner scanner = new Scanner(System.in);
 
